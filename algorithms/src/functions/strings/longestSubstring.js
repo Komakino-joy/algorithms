0d548a57e26eb4fb4d1495b9d23a4dc8cd490b4b
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-07-12 19:19:53 -0700

    added longest substring optimal solution

--- algorithms/src/functions/strings/longestSubstring.js
+++ algorithms/src/functions/strings/longestSubstring.js
@@ -6,8 +6,10 @@
 // 2. Does case matter ? 
 // - No 
 
-
-const findLongestSubstring = (str) => {
+// Complexity
+// Space = O(n)
+// Time = O(n^2)
+const findLongestSubstringBruteforce = (str) => {
     if (str.length <= 1) {
         return str.length;
     }
@@ -30,5 +32,34 @@ const findLongestSubstring = (str) => {
             }
         }
     }
+    return longest;
+}
+
+
+// Complexity
+// Space = O(n)
+// Time = O(n)
+const findLongestSubstringOptimal = (str) => {
+    if (str.length <= 1) {
+        return str.length;
+    }
+
+    const seenChar = {};
+    let left = 0;
+    let longest = 0;
+
+    for (let right=0; right < s.length; right++) {
+        const currentChar = s[right];
+        const prevSeenChar = seenChar[currentChar];
+
+        if (prevSeenChar >= left) {
+            left = prevSeenChar + 1;
+        }
+
+        seenChar[currentChar] = right;
+
+        longest = Math.max(longest, right - left + 1);
+    }
+
     return longest;
 }
