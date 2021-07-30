d4e97f66e76e010731e03d8fb917758362e274d2
Bryan Alvarez <alvarez.bryan79@yahoo.com>
2021-07-29 21:41:46 -0700

    implemented BFS visual 2D search

--- algorithms/src/components/islands/islands.component.jsx
+++ algorithms/src/components/islands/islands.component.jsx
@@ -4,89 +4,114 @@ import './islands.styles.css';
 
 const Islands = () => {
 
-    const [myMatrix, setMyMatrix] = useState(
+
+    const [matrix, setMatrix] = useState(
         [[1, 1, 1, 1, 0],
         [1, 1, 0, 1, 0],
         [1, 1, 0, 0, 1],
-        [0, 0, 0, 1, 1]]);
+        [0, 0, 0, 1, 1]]
+    )        
 
-    const [islands, setIslands] = useState(0);
+    let [totalIslands, setTotalIslands] = useState(0);
 
-    const directions = [
-        [-1, 0],
-        [0,  1],
-        [1,  0],
-        [0, -1]
-    ]
+    const directions = {
+        0:{
+            coordinates:[-1, 0],
+            direction: 'left',
+        },
+        1:{
+            coordinates:[0,  1],
+            direction: 'up',
+        },
+        2:{
+            coordinates:[1,  0],
+            direction: 'right',
+        },
+        3:{
+            coordinates:[0, -1],
+            direction: 'down',
+        },
+    }
     
-    
-    const numOfIslands = () => {      
 
-        const newMatrix = Array.from(myMatrix)
-
-        if (newMatrix.length === 0) return 0;
-    
-        let islandCount = 0;
-    
-        for (let row=0; row<newMatrix.length; row++) {
-            for (let col=0; col<newMatrix[0].length; col++) {
-                
-                if (newMatrix[row][col] === 1) {
-                    setIslands(islandCount++);
-                    // newMatrix[row][col]=0
-
-                    newMatrix[row][col] = 0
-                    setMyMatrix(newMatrix)
-
-         
-                    const queue = [];
-                    queue.push([row],[col]);
-    
-                    while (queue.length) {
-                        const currentPos = queue.shift();
-                        const currentRow = currentPos[0];
-                        const currentCol = currentPos[1];
-    
-                        for (let i=0; i<directions.length; i++) {
-                            const currentDir = directions[i];
-                            const nextRow = currentRow + currentDir[0];
-                            const nextCol = currentCol + currentDir[1];
-    
-                            if(nextRow < 0 || nextRow >= newMatrix.length || nextCol < 0 || nextCol >= newMatrix[0].length) {
-                                continue;
-                            }
-    
-                            if (newMatrix[nextRow][nextCol] === 1) {
-                                console.log(newMatrix[0])
-                                queue.push([nextRow, nextCol]);
-                                newMatrix[nextRow][nextCol] = 0;
-                    
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
+    const sleep = ms => {
+        return new Promise(resolve => setTimeout(resolve, ms))
     }
 
+    const updateMatrix = async(matrix, row, col) => {
+        const copyOfMatrix = [...matrix];
+        await sleep(100);
+        copyOfMatrix[row][col] = 0;
+        // console.log(matrix[0])
+        // console.log(matrix[1])
+        // console.log(matrix[2])
+        // console.log(matrix[3])
+        setMatrix(copyOfMatrix)
+    }
+
+    const numOfIslands = async () => {
+        let islands = 0;
+      
+        if (matrix.length === 0) {
+          return 0;
+        }
+      
+        for (let row = 0; row < matrix.length; row++) {
+      
+          for (let col=0; col < matrix[0].length; col++) {
+      
+            if (matrix[row][col] === 1) {
+              islands++;
+              
+              await updateMatrix(matrix, row, col);
+              
+              const queue = [];
+              queue.push([row,col]);
+      
+              while (queue.length) {
+                const currentPos = queue.shift();
+                const currentRow = currentPos[0]
+                const currentCol = currentPos[1]
+   
+      
+                for (let i = 0; i < Object.keys(directions).length; i++) {
+                  const currentDir = directions[i].coordinates;
+                  console.log(directions[i].direction)
+                  const nextRow = currentRow + currentDir[0];
+                  const nextCol = currentCol + currentDir[1];
+      
+                  if (nextRow < 0 || nextRow >= matrix.length || nextCol < 0 || nextCol >= matrix[0].length) {
+                    continue
+                  }
+      
+                  if (matrix[nextRow][nextCol] === 1) {
+                    queue.push([nextRow, nextCol]);
+                    await updateMatrix(matrix, nextRow, nextCol);
+                  }
+                }
+              }
+            }
+          }
+        }
+        setTotalIslands(islands);
+      }
+      
     return (
         <div className='islands-container'>
             <div className='matrix-container'>
-
-                {
-                    myMatrix.map((row, idx) => (
+                {matrix &&
+                    matrix.map((row, idx) => (
                         row.map((col, idx) => (
                             <div key={idx} className={`${col === 0 ? 'water' : 'land'} matrix-square`}>{col}</div>
                         ))
                     ))
                 }
             </div>
-            <button onClick={numOfIslands} >Find Islands</button>
-            <button onClick={() => console.log(myMatrix)} >My Matrix</button>
-            <p>There are {islands} islands on this map.</p>
-            <p>{myMatrix}</p>
-        </div>
+            <button onClick={() => numOfIslands(matrix)} >Find Islands</button>
+            <button onClick={() => console.log(matrix)} >My Matrix</button>
+            <p>There are {totalIslands} islands on this map.</p>
+                
+            </div>
     )
 }
 
